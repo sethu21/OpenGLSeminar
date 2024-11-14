c020e3b5071ee3965147d2bf6ab15bd40dada10b
s22sethselv <s22sethselv@C100W-2RLUH98AN>
2024-11-14 16:11:17 +0200

    Add project files.

---
+++ DrawingTeapot/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpGL;
+using SharpGL.SceneGraph.Primitives;
+
+namespace DrawingTeapot
+{
+    public partial class Form1 : Form
+    {
+        OpenGL gl;
+        float rot = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openGLControl1_OpenGLDraw(object sender, SharpGL.RenderEventArgs args)
+        {
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+            gl.LoadIdentity();
+            gl.Translate(0, 1, -8);
+            gl.Rotate(rot, 1, 0.5, 1);
+            Teapot tp = new Teapot();
+            tp.Draw(gl, 14, 1, OpenGL.GL_FILL);
+            gl.End();
+
+            rot += 2;
+        }
+
+        private void openGLControl1_OpenGLInitialized(object sender, EventArgs e)
+        {
+            gl = openGLControl1.OpenGL;
+
+            gl.Enable(OpenGL.GL_DEPTH_TEST);
+            float[] global_ambient = new float[] { 0.5f, 0.5f, 0.5f, 0.5f };
+            float[] light0pos = new float[] { 0.0f, 5.0f, -10.0f, 1.0f };
+            float[] light0ambient = new float[] { 0.2f, 0.2f, 0.2f, 1.0f };
+            float[] light0diffuse = new float[] { 0.1f, 0.9f, 0.3f, 1.0f };
+            float[] light0specular = new float[] { 0.1f, 0.8f, 0.8f, 1.0f };
+            gl.LightModel(OpenGL.GL_LIGHT_MODEL_AMBIENT, global_ambient);
+            gl.Light(OpenGL.GL_LIGHT0, OpenGL.GL_POSITION, light0pos);
+            gl.Light(OpenGL.GL_LIGHT0, OpenGL.GL_AMBIENT, light0ambient);
+            gl.Light(OpenGL.GL_LIGHT0, OpenGL.GL_DIFFUSE, light0diffuse);
+            gl.Light(OpenGL.GL_LIGHT0, OpenGL.GL_SPECULAR, light0specular);
+            gl.Enable(OpenGL.GL_LIGHTING);
+            gl.Enable(OpenGL.GL_LIGHT0);
+            gl.ShadeModel(OpenGL.GLU_FLAT);
+
+        }
+    }
+}
---
+++ OpenGLSeminar/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpGL;
+using SharpGL.SceneGraph;
+using SharpGL.SceneGraph.Assets;
+
+namespace OpenGLSeminar
+{
+    public partial class Form1 : Form
+    {
+        OpenGL gl;
+        Texture texture = new Texture();
+
+        float rcube = 0;
+        float rpyr = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            gl = openGLControl1.OpenGL;
+            gl.Enable(OpenGL.GL_TEXTURE_2D);
+            texture.Create(gl, "crateTexture.jpg");
+
+        }
+
+        private void openGLControl1_OpenGLDraw(object sender, SharpGL.RenderEventArgs args)
+        {
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+            gl.LoadIdentity();
+
+            gl.Translate(-1.5f, 0.0f, -6.0f);
+            gl.Rotate(rpyr, 0.0f, 1.0f, 0.0f);
+
+            gl.Begin(OpenGL.GL_TRIANGLES);
+
+            gl.Color(1.0f, 0.0f, 0.0f);
+            gl.Vertex(0, 1, 0);
+            gl.Color(0.0f, 1.0f, 0.0f);
+            gl.Vertex(-1, -1, 1);
+            gl.Color(0.0f, 0.0f, 1.0f);
+            gl.Vertex(1, -1, 1);
+
+            gl.Color(1.0f, 0.0f, 0.0f);
+            gl.Vertex(0, 1, 0);
+            gl.Color(0.0f, 0.0f, 1.0f);
+            gl.Vertex(1.0f, -1.0f, 1.0f);
+            gl.Color(0.0f, 1.0, 0.0f);
+            gl.Vertex(1.0f, -1.0f, -1.0f);
+
+            gl.Color(1.0f, 0.0f, 0.0f);
+            gl.Vertex(0.0f, 1.0f, 0.0f);
+            gl.Color(0.0f, 1.0f, 0.0f);
+            gl.Vertex(1.0f, -1.0f, -1.0f);
+            gl.Color(0.0f, 0.0f, 1.0f);
+            gl.Vertex(-1.0f, -1.0f, -1.0f);
+
+            gl.Color(1.0f, 0.0f, 0.0f);
+            gl.Vertex(0, 1, 0);
+            gl.Color(0.0f, 0.0f, 1.0f);
+            gl.Vertex(-1.0f, -1.0f, -1.0f);
+            gl.Color(0.0f, 1.0f, 0.0f);
+            gl.Vertex(-1.0f, -1.0f, 1.0f);
+            gl.End();
+
+            gl.LoadIdentity();
+
+            gl.Translate(1.5f, 0.0f, -7.0f);
+            gl.Rotate(rcube, 1.0f, 1.0f, 1.0f);
+
+            texture.Bind(gl);
+            gl.Begin(OpenGL.GL_QUADS);
+
+            gl.Color(1.0f, 1.0f, 1.0f);
+            gl.TexCoord(0, 0); gl.Vertex(1, 1, -1);
+            gl.TexCoord(1, 0); gl.Vertex(-1, 1, -1);
+            gl.TexCoord(1, 1); gl.Vertex(-1, 1, 1);
+            gl.TexCoord(0, 1); gl.Vertex(1, 1, 1);
+
+            //gl.Color(1.0f, 0.5f, 0.0f);
+            gl.TexCoord(0, 0); gl.Vertex(1, -1, 1);
+            gl.TexCoord(1, 0); gl.Vertex(-1, -1, 1);
+            gl.TexCoord(1, 1); gl.Vertex(-1, -1, -1);
+            gl.TexCoord(0, 1); gl.Vertex(1, -1, -1);
+
+            //gl.Color(1.0f, 0.0f, 0.0f);
+            gl.TexCoord(0, 0); gl.Vertex(1, 1, 1);
+            gl.TexCoord(1, 0); gl.Vertex(-1, 1, 1);
+            gl.TexCoord(1, 1); gl.Vertex(-1, -1, 1);
+            gl.TexCoord(0, 1); gl.Vertex(1, -1, 1);
+
+            //gl.Color(1.0f, 1.0f, 0.0f);
+            gl.TexCoord(0, 0); gl.Vertex(1, -1, -1);
+            gl.TexCoord(1, 0); gl.Vertex(-1, -1, -1);
+            gl.TexCoord(1, 1); gl.Vertex(-1, 1, -1);
+            gl.TexCoord(0, 1); gl.Vertex(1, 1, -1);
+
+            //gl.Color(1.0f, 0.0f, 1.0f);
+            gl.TexCoord(0, 0); gl.Vertex(-1, 1, 1);
+            gl.TexCoord(1, 0); gl.Vertex(-1, 1, -1);
+            gl.TexCoord(1, 1); gl.Vertex(-1, -1, -1);
+            gl.TexCoord(0, 1); gl.Vertex(-1, -1, 1);
+
+            //gl.Color(0.0f, 0.0f, 1.0f);
+            gl.TexCoord(0, 0); gl.Vertex(1, 1, -1);
+            gl.TexCoord(1, 0); gl.Vertex(1, 1, 1);
+            gl.TexCoord(1, 1); gl.Vertex(1, -1, 1);
+            gl.TexCoord(0, 1); gl.Vertex(1, -1, -1);
+
+
+
+            gl.End();
+
+            rpyr += 3;
+            rcube += 2;
+        }
+
+        private void openGLControl1_OpenGLInitialized(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
